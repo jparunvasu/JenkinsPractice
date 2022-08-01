a8a658aa40332c2e6edbd0864d88a815eb7fa45d
jparunvasu <110111378+jparunvasu@users.noreply.github.com>
2022-08-01 18:25:28 +0530

    Initial commit

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class HelloGuys{
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey Agni");
+		System.out.println("Hey Riya");
+		System.out.println("Hey Duck");
+		System.out.println("Hey Arya");
+}
+}
